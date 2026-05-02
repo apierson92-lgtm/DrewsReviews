5ab732c7b048400591a587e5533ee0f5ddaf8798
apierson92-lgtm <apierson92@gmail.com>
2026-05-02 11:40:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,48 @@
-import express from "express";
-import cors from "cors";
-import yahooFinance from "yahoo-finance2";
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(cors());
 
 const TICKERS = [
-  "NVDA","MSFT","AAPL","AMZN","GOOGL","META","BRK-B","JPM","V","JNJ",
-  "PG","UNH","XOM","CVX","TSLA","COST","HD","ABBV","AMD","CRM","LLY",
-  "MA","NEE","PFE","T","UBER","SHOP","PLTR","ARM","SOFI","NIO","RIVN",
-  "COIN","HOOD","RBLX","SNAP","PINS","LYFT","DKNG","PENN","MGM","LVS",
-  "WYNN","CZR","MPWR","ENPH","FSLR","SEDG","RUN","BE","PLUG","BLDP",
-  "QS","CHPT","LCID","FSR","GOEV","NKLA","HYLN","WKHS","RIDE","SOLO",
-  "F","GM","STLA","TM","HMC","NSANY","VWAGY","BMWYY","MBGAF","RACE",
-  "GS","MS","BAC","C","WFC","USB","PNC","TFC","COF","AXP","DFS","SYF",
-  "PYPL","SQ","AFRM","UPST","LC","SOFI","NU","PAGS","STNE","MELI",
-  "SE","GRAB","GOJEK","BIDU","JD","PDD","BABA","TCEHY","NTES","BEKE",
-  "ZM","DOCU","TWLO","OKTA","DDOG","SNOW","MDB","ESTC","CFLT","NET",
-  "TEAM","ATLASSIAN","ZS","PANW","CRWD","S","FTNT","CHKP","CYBR","VRNS",
-  "INTC","QCOM","AVGO","TXN","MCHP","ADI","KLAC","LRCX","AMAT","ASML",
-  "TSM","MU","WDC","STX","PSTG","NTAP","HPE","HPQ","DELL","SMCI",
-  "IBM","ORCL","SAP","NOW","ADBE","INTU","ANSS","CDNS","SNPS","PTC",
-  "AMGN","GILD","REGN","VRTX","BIIB","ILMN","IDXX","ISRG","SYK","BSX",
-  "MDT","ABT","TMO","DHR","A","BIO","MTD","WAT","TECH","HOLX",
-  "DIS","NFLX","WBD","PARA","FOX","NYT","SPOT","TTWO","EA","ATVI",
-  "NKE","ADDYY","PUMA","UAA","LULU","VFC","PVH","RL","TPR","CPRI",
-  "MCD","SBUX","YUM","QSR","DRI","CMG","SHAK","WING","TXRH","CAKE",
-  "AMZN","WMT","TGT","COST","KR","ACI","SFM","CASY","BJ","GO",
-  "CVS","WBA","RAD","CAH","MCK","ABC","HSIC","PDCO","XRAY","VTRS",
-  "BA","LMT","RTX","NOC","GD","L3H","HII","TDG","HEI","AXON",
-  "CAT","DE","CMI","PCAR","URI","AGCO","CNHI","TITN","ASTE","TEX",
-  "BHP","RIO","VALE","FCX","NEM","AEM","WPM","KGC","AGI","OR"
+  "MMM","AOS","ABT","ABBV","ACN","ADBE","AMD","AES","AFL","A","APD","ABNB","AKAM","ALB","ARE","ALGN","ALLE","LNT","ALL","GOOGL","GOOG","MO","AMZN","AMCR","AEE","AAL","AEP","AXP","AIG","AMT","AWK","AMP","AME","AMGN","APH","ADI","ANSS","AON","APA","AAPL","AMAT","APTV","ACGL","ADM","ANET","AJG","AIZ","T","ATO","ADSK","ADP","AZO","AVB","AVY","AXON","BKR","BALL","BAC","BK","BBWI","BAX","BDX","WRB","BBY","BIO","TECH","BIIB","BLK","BX","BA","BCR","BSX","BMY","AVGO","BR","BRO","BF.B","BLDR","BXP","CHRW","CDNS","CZR","CPT","CPB","COF","CAH","KMX","CCL","CARR","CTLT","CAT","CBOE","CBRE","CDW","CE","COR","CNC","CNP","CF","CRL","SCHW","CHTR","CVX","CMG","CB","CHD","CI","CINF","CTAS","CSCO","C","CFG","CLX","CME","CMS","KO","CTSH","CL","CMCSA","CAG","COP","ED","STZ","CEG","COO","CPRT","GLW","CPAY","CTVA","CSGP","COST","CTRA","CRWD","CCI","CSX","CMI","CVS","DHI","DHR","DRI","DVA","DAY","DECK","DE","DAL","DVN","DXCM","FANG","DLR","DFS","DG","DLTR","D","DPZ","DOV","DOW","DHC","DTE","DUK","DD","EMN","ETN","EBAY","ECL","EIX","EW","EA","ELV","LLY","EMR","ENPH","ETR","EOG","EPAM","EQT","EFX","EQIX","EQR","ESS","EL","ETSY","EG","EVRG","ES","EXC","EXPE","EXPD","EXR","XOM","FFIV","FDS","FICO","FAST","FRT","FDX","FIS","FITB","FSLR","FE","FI","FMC","F","FTNT","FTV","FOXA","FOX","BEN","FCX","GRMN","IT","GE","GEHC","GEV","GEN","GNRC","GD","GIS","GM","GPC","GILD","GPN","GL","GDDY","GS","HAL","HIG","HAS","HCA","DOC","HSIC","HSY","HES","HPE","HLT","HOLX","HD","HON","HRL","HST","HWM","HPQ","HUBB","HUM","HBAN","HII","IBM","IEX","IDXX","ITW","INCY","IR","PODD","INTC","ICE","IFF","IP","IPG","INTU","ISRG","IVZ","INVH","IQV","IRM","JBAL","JKHY","J","JBL","JNPR","JPM","JNPR","K","KVUE","KDP","KEY","KEYS","KMB","KIM","KMI","KLAC","KHC","KR","LHX","LH","LRCX","LW","LVS","LDOS","LEN","LIN","LYV","LKQ","LMT","L","LOW","LULU","LYB","MTB","MRO","MPC","MKTX","MAR","MMC","MLM","MAS","MA","MTCH","MKC","MCD","MCK","MDT","MET","META","MTD","MGM","MCHP","MU","MSFT","MAA","MRNA","MHK","MOH","TAP","MDLZ","MPWR","MNST","MCO","MS","MOS","MSI","MSCI","NDAQ","NTAP","NFLX","NEM","NWSA","NWS","NEE","NKE","NI","NDSN","NSC","NTRS","NOC","NCLH","NRG","NUE","NVDA","NVR","NXPI","ORLY","OXY","ODFL","OMC","ON","OKE","ORCL","OTIS","PCAR","PKG","PLTR","PANW","PARA","PH","PAYX","PAYC","PYPL","PNR","PEP","PFE","PCG","PM","PSX","PNW","PNC","POOL","PPG","PPL","PFG","PG","PGR","PLD","PRU","PEG","PTC","PSA","PHM","QRVO","PWR","QCOM","DGX","RL","RJF","RTX","O","REG","REGN","RF","RSG","RMD","RVTY","ROK","ROL","ROP","ROST","RCL","SPGI","CRM","SBAC","SLB","STX","SRE","NOW","SHW","SPG","SWKS","SJM","SW","SNA","SOLV","SO","LUV","SWK","SBUX","STT","STLD","STE","SYK","SMCI","SYF","SNPS","SYY","TMUS","TROW","TTWO","TPR","TRGP","TGT","TEL","TDY","TFX","TER","TSLA","TXN","TMO","TJX","TSCO","TT","TDG","TRV","TRMB","TFC","TYL","TSN","USB","UBER","UDR","ULTA","UNP","UAL","UPS","URI","UNH","UHS","VLO","VTR","VLTO","VRSN","VRSK","VZ","VRTX","VTRS","VICI","V","VST","VMC","WRK","WAB","WBA","WMT","DIS","WBD","WM","WAT","WEC","WFC","WELL","WST","WDC","WY","WHR","WMB","WTW","GWW","WYNN","XEL","XYL","YUM","ZBRA","ZBH","ZTS"
 ];
+
+async function fetchBatch(tickers) {
+  const symbols = tickers.join(",");
+  const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbols}&fields=symbol,longName,regularMarketPrice,regularMarketChangePercent,marketCap,forwardPE,earningsGrowth,profitMargins,debtToEquity,dividendYield,beta,sector`;
+  const response = await fetch(url, { headers: { "User-Agent": "Mozilla/5.0" } });
+  const data = await response.json();
+  return data?.quoteResponse?.result || [];
+}
+
+function chunkArray(arr, size) {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += size) chunks.push(arr.slice(i, i + size));
+  return chunks;
+}
 
 app.get("/stocks", async (req, res) => {
   try {
-    const results = await Promise.allSettled(
-      TICKERS.map(ticker =>
-        yahooFinance.quote(ticker).then(q => ({
-          ticker: q.symbol,
-          name: q.longName || q.shortName || q.symbol,
-          sector: q.sector || "Unknown",
-          price: q.regularMarketPrice || 0,
-          marketCap: q.marketCap ? +(q.marketCap / 1e9).toFixed(1) : 0,
-          pe: q.forwardPE ? +q.forwardPE.toFixed(1) : null,
-          ebitdaGrowth: q.earningsGrowth ? +(q.earningsGrowth * 100).toFixed(1) : null,
-          revenueGrowth: q.revenueGrowth ? +(q.revenueGrowth * 100).toFixed(1) : null,
-          margin: q.profitMargins ? +(q.profitMargins * 100).toFixed(1) : null,
-          debtEquity: q.debtToEquity ? +(q.debtToEquity / 100).toFixed(2) : null,
-          divYield: q.dividendYield ? +(q.dividendYield * 100).toFixed(2) : 0,
-          beta: q.beta ? +q.beta.toFixed(2) : null,
-          volatility: q["52WeekChange"] ? +(Math.abs(q["52WeekChange"]) * 100).toFixed(1) : null,
-        }))
-      )
-    );
+    const batches = chunkArray(TICKERS, 100);
+    const results = await Promise.all(batches.map(fetchBatch));
+    const quotes = results.flat();
 
-    const stocks = results
-      .filter(r => r.status === "fulfilled" && r.value.marketCap > 0)
-      .map(r => r.value);
+    const stocks = quotes.map(q => ({
+      ticker: q.symbol,
+      name: q.longName || q.symbol,
+      sector: q.sector || "Unknown",
+      price: q.regularMarketPrice || 0,
+      dailyReturn: q.regularMarketChangePercent ? +q.regularMarketChangePercent.toFixed(2) : 0,
+      marketCap: q.marketCap ? +(q.marketCap / 1e9).toFixed(1) : 0,
+      pe: q.forwardPE ? +q.forwardPE.toFixed(1) : null,
+      ebitdaGrowth: q.earningsGrowth ? +(q.earningsGrowth * 100).toFixed(1) : null,
+      margin: q.profitMargins ? +(q.profitMargins * 100).toFixed(1) : null,
+      debtEquity: q.debtToEquity ? +(q.debtToEquity / 100).toFixed(2) : null,
+      divYield: q.dividendYield ? +(q.dividendYield * 100).toFixed(2) : 0,
+      beta: q.beta ? +q.beta.toFixed(2) : null,
+    })).filter(s => s.price > 0);
 
     res.json(stocks);
   } catch (e) {
